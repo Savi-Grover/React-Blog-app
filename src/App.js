df73995e12087fd1b8a6eb41c4a6212f3255718f
Savi Grover <savig447@gmail.com>
2022-12-21 12:06:24 -0500

    Outputting List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import NavBar from './Navbar';
 import Home from './Home';
 import Article from './Article';
+import { useState } from 'react';
+
 function App() {
 
   //declare title as const
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,14 @@
+import { useState } from 'react';
 
 const Home = () => {
+
+    const[blogs,setBlogs]= useState([
+{ title: 'My new website', body:'loren ipsum...', author: 'mario', id:1},
+{ title: 'web dev tool tips', body:'loren ipsum...', author: 'luigi', id:2},
+{ title: 'Pro tips to web customisation', body:'loren ipsum...', author: 'yoshi', id:3}
+
+    ]);
+
 const handleClick=()=> {
     console.log('hello all');
 }
@@ -10,11 +19,23 @@ const handleClickAgain=(name)=> {
 
     return (  
 <div className="home">
+
+{blogs.map((blogtoshow)=>(
+<div className="blog-preview" key={blogtoshow.id}>
+<h2>{ blogtoshow.title}</h2>
+<p>Written by {blogtoshow.author}</p>
+</div>
+)
+)}
+
+
     {/*<h2>Homepage< a href="/" style={{color: "purple"}}>Content</a></h2>*/}
     <button onClick={handleClick}>Click me</button>
     <button onClick={
         ()=>{handleClickAgain('savi')}
         }>Click me again</button>
+
+
 </div>
     );
 }
