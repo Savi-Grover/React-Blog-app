fdd438a0907c6846ca2c6dbca6572a793bca4bc8
Savi Grover <savig447@gmail.com>
2022-12-22 09:47:49 -0500

    Exception Handling

--- src/Home.js
+++ src/Home.js
@@ -5,30 +5,40 @@ const Home = () => {
 
     const[blogs,setBlogs]= useState(null);
     const[isLoading,setLoading]=useState(true);
+    const[isError,setError]=useState(null);
 
-    const [name, setName]=useState('mario');
+    //const [name, setName]=useState('mario');
 //     const handleDelete = (id)=>{
 //     const newBlogs=blogs.filter(blog =>blog.id!==id);
 //     setBlogs(newBlogs)
     
 // }
-
 useEffect(()=> {
 setTimeout(() => {
     fetch('http://localhost:8000/blogs')
 .then(res => {
+    console.log(res);
+    if (!res.ok){
+throw Error('could not fetch the data from resource')
+    }
     return res.json();
 })
 .then( data =>{
     console.log(data);
     setBlogs(data);
     setLoading(false);
-}); }, 1000);
+    setError(null);
+}).catch(err => {
+    //console.log(err.message);
+    setError(err.message);
+})
+}, 1);
 }, []);
     return (  
 <div className="home">
 {isLoading && <div>Loading....</div>}
 {blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
+{isError && <div>{ isError }</div>}
 {/*<BlogList blogs={blogs.filter((blog)=>blog.author==='mario')} title="All Blogs!" handleDelete={handleDelete}/> */}
 
 {/* <button onClick={() => setName('luigi')}>Name Change</button>
