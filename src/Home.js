7d93da073dfd1505c6b0c2f9005d565c128aadd8
Savi Grover <savig447@gmail.com>
2022-12-21 20:14:08 -0500

    UseEffect for Fetching real time json data

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,8 @@ import { useEffect } from 'react';
 import BlogList from './BlogList';
 const Home = () => {
 
-    const[blogs,setBlogs]= useState([
-{ title: 'My new website', body:'loren ipsum...', author: 'mario', id:1},
-{ title: 'web dev tool tips', body:'loren ipsum...', author: 'luigi', id:2},
-{ title: 'Pro tips to web customisation', body:'loren ipsum...', author: 'yoshi', id:3}
+    const[blogs,setBlogs]= useState(null);
 
-    ]);
     const [name, setName]=useState('mario');
     const handleDelete = (id)=>{
     const newBlogs=blogs.filter(blog =>blog.id!==id);
@@ -17,14 +13,20 @@ const Home = () => {
 }
 
 useEffect(()=> {
-console.log('use effect ran');
-console.log(blogs);
-}, [name]
+fetch('http://localhost:8000/blogs')
+.then(res => {
+    return res.json();
+})
+.then( data =>{
+    console.log(data);
+    setBlogs(data);
+})
+}, []
 );
     return (  
 <div className="home">
 
-<BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/> 
+{blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/> }
 {/*<BlogList blogs={blogs.filter((blog)=>blog.author==='mario')} title="All Blogs!" handleDelete={handleDelete}/> */}
 <button onClick={() => setName('luigi')}>Name Change</button>
 <p>{name}</p>
