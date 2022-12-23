9059b18b09168b9bcb72890535f5073919fe54f0
Savi Grover <savig447@gmail.com>
2022-12-22 22:01:46 -0500

    Custom hook for blog details

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
+
 const BlogDetail = () => {
     const{ id }=useParams();
-    
+    const {data : blog, isError, isLoading }= useFetch('http://localhost:8000/blogs/' + id);
+   
     return ( 
         <div className="blog-details">
-        <h2>
-          Blog details - { id }
-        </h2>
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>{ isError }</div>}
+        { blog && (
+          <article>
+            <h2>{ blog.title }</h2>
+            <p>Written By { blog.author}</p>
+            <div>{blog.body}</div>
+          </article>
+        )}
         </div>
      );
 }
